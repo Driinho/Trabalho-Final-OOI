2d632a24009c2b2c21cad004f1dfc85a8e7e04b3
Driinho <pedro.nascimento.henrique2003@gmail.com>
2022-09-01 13:32:16 -0300

    Criada classe Pessoa

--- src/main/java/ifpr/pgua/eic/escola/App.java
+++ src/main/java/ifpr/pgua/eic/escola/App.java
@@ -1,22 +1,8 @@
 package ifpr.pgua.eic.escola;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import java.io.IOException;
-
-import ifpr.pgua.eic.escola.utils.BaseAppNavigator;
-
-/**
- * JavaFX App
- */
-public class App extends BaseAppNavigator {
+public class App {
 
     public static void main(String[] args) {
-        launch();
-    }
 
+    }
 }
