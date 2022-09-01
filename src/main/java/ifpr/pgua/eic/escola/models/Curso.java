4df1f6a3d82ea7f6be2335a84f48c92f44990ced
Driinho <pedro.nascimento.henrique2003@gmail.com>
2022-09-01 14:03:15 -0300

    Criada classe curso mas não foram implementados os metodos para matricula e desmatricula de aluno

---
+++ src/main/java/ifpr/pgua/eic/escola/models/Curso.java
@@ -0,0 +1,81 @@
+package ifpr.pgua.eic.escola.models;
+
+import java.util.ArrayList;
+
+public class Curso {
+
+    private int codigo;
+    private String nome;
+    private String descricao;
+    private int cargaHoraria;
+    private Professor professor;
+    private ArrayList<Aluno> alunos;
+
+    public Curso(int codigo, String nome, String descricao, int cargaHoraria, Professor professor) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.descricao = descricao;
+        this.cargaHoraria = cargaHoraria;
+        this.professor = professor;
+
+        alunos = new ArrayList<>();
+    }
+
+    public boolean matricula(Aluno aluno) {
+
+        return false;
+    }
+
+    public boolean desmatricula(String cpf) {
+
+        return false;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public Professor getProfessor() {
+        return professor;
+    }
+
+    public void setProfessor(Professor professor) {
+        this.professor = professor;
+    }
+
+    public ArrayList<Aluno> getAlunos() {
+        return alunos;
+    }
+
+    public void setAlunos(ArrayList<Aluno> alunos) {
+        this.alunos = alunos;
+    }
+}
